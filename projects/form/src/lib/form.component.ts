75710728d910f822203279716cf97d9f0232f7f7
hzj <hezhijian249@qq.com>
2024-04-28 22:54:03 +0800

    feat: 创建form组件

---
+++ projects/form/src/lib/form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'an-form',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      form works!
+    </p>
+  `,
+  styles: ``
+})
+export class FormComponent {
+
+}
